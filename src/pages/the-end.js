cd3779294f6a932cff6e2778247f730b04081ca6
Tim Signore <timsignore@gmail.com>
2019-11-22 13:57:24 +0000

    Removed final button

--- src/pages/the-end.js
+++ src/pages/the-end.js
@@ -4,9 +4,8 @@ import Headertext from "../components/header_text"
 const theEnd = () => (
   <React.Fragment>
     <Headertext headline="The facilitator will take you to the quote page" />
-    <a href="https://guardian.co.uk" target="_blank" rel="noopener noreferrer">
-      <button classs="primary">Go to Guardian</button>
-    </a>
+    
+   
   </React.Fragment>
 ) 
 
